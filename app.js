2c0b6825fd434e76870ac2874c4eb1718625c9c1
Milica Gareski <milicakiprovska77@gmail.com>
2024-02-04 00:18:29 +0100

    Added cors

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
+    origin: [
+      "http://localhost:5500",
+      "http://127.0.0.1:5500",
+      "https://milicagareski.github.io",
+    ],
     credentials: true,
   })
 );
